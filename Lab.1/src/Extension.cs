2b6a1d241c3343a54c38d5752dce6625519a5bd3
EvdAnton <47704348+EvdAnton@users.noreply.github.com>
2020-11-22 20:52:27 +0300

    [Lab_1] feat(objReader): Add obj reader

--- Lab.1/src/Extension.cs
+++ Lab.1/src/Extension.cs
@@ -7,8 +7,7 @@ namespace Lab._1
     {
         public static void Add(this int[] vertex, int i, string[] parts, int index)
         {
-            if(parts.Length < index)
-                return;
+            if(parts.Length < index) return;
 
             var success = int.TryParse(parts[index], out var vIndex);
             if (!success) throw new ArgumentException("Could not parse parameter as int");
@@ -18,14 +17,10 @@ namespace Lab._1
 
         public static decimal GetDecimalByIndex(this string[] data, int index, string parameterNaming = "")
         {
-            if (data.Length < index)
-                return default;
+            if (data.Length < index) return default;
 
             var success = decimal.TryParse(data[index], NumberStyles.Any, CultureInfo.InvariantCulture, out var x);
-            if (!success)
-            {
-                throw new ArgumentException($"Could not parse {parameterNaming} parameter as decimal");
-            }
+            if (!success) throw new ArgumentException($"Could not parse {parameterNaming} parameter as decimal");
 
             return x;
         }
